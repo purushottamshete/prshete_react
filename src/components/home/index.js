0cb97b752d86fa280eda63422a9797bde922ac43
Purushottam <purushottamshete@gmail.com>
2024-01-03 19:02:55 +0530

    Updating SEO tags

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Banner from './banner'
 import Porjects from '../projects/projects';
+import { Helmet } from 'react-helmet';
 
 const Home = () => {
     return (
         <>
+            <Helmet>
+                <title>Prshete.com | Home </title>
+                <meta name="description" content="Freelancer Python Backend Developer" />
+                <meta name="keywords" content="Freelancer Python Backend Developer, Remote Python Backend Developer" />
+                <meta name="author" content="Purushottam Shete" />
+            </Helmet>
             <Banner />
             <Porjects />
         </>
