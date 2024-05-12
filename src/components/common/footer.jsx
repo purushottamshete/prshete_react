f55de6d5fe3c13142b3a3f08c88317965282c794
Purushottam <purushottamshete@gmail.com>
2024-05-12 11:20:47 +0530

    Footer beatification

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -18,7 +18,8 @@ export default function Footer() {
           theme.palette.mode === "light"
             ? theme.palette.grey[200]
             : theme.palette.grey[800],
-        p: 6,
+        py: 4,
+        px: 1
       }}
     >
       <Container maxWidth="lg">
